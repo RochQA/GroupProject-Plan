131d923f92612b7d3cc3c6ecbec491650bb480a1
GeorgeH_games <georgeh4@live.co.uk>
2019-04-01 15:09:17 +0100

    Dan has done enough tests for plan api

--- src/main/java/com/qa/plan/service/PlanService.java
+++ src/main/java/com/qa/plan/service/PlanService.java
@@ -1,97 +1,59 @@
 package com.qa.plan.service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Date;
-import java.time.Year;
-import java.time.YearMonth;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.qa.plan.entities.Plan;
-import com.qa.plan.repository.PlanRepository;
 
 @Service
 public class PlanService {
-	
+
 	public String checkValid(Plan plan, List<Plan> plans) {
-		String dateRes= checkDate(plan);
-		if(dateRes.equals("Valid")) {
+		String dateRes = checkDate(plan);
+		if (dateRes.equals("Valid")) {
 			String planRes = checkAvailible(plan, plans);
-			if(planRes.equals("Valid")){
+			if (planRes.equals("Valid")) {
 				return "Valid";
-			}else return planRes;			
-		}else return dateRes;
+			} else
+				return planRes;
+		} else
+			return dateRes;
 	}
-	
-	
+
 	public String checkDate(Plan plan) {
 		Date localDate = java.sql.Date.valueOf(LocalDate.now());
-		if(plan.getStartDate().before(localDate)) {
+		if (plan.getStartDate().before(localDate)) {
 			return "Date in past!";
-		}else if(plan.getStartDate().after(plan.getEndDate())) {
-			return "End date before start date!";		
+		} else if (plan.getStartDate().after(plan.getEndDate())) {
+			return "End date before start date!";
 //		} else if(!(plan.getMonth() >= Calendar.getInstance().get(Calendar.MONTH) + 1) || !(plan.getMonth() <= 12)) {
 //			return "Month not valid!";
 //		} else if(!(plan.getDay() <= YearMonth.of(plan.getYear(), plan.getMonth()).lengthOfMonth() || !(plan.getDay() >= Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + 1))) {
 //			return "Day not valid!";
-		}else return "Valid";
+		} else
+			return "Valid";
 	}
-	
+
 	public String checkAvailible(Plan plan, List<Plan> plans) {
 		Date startDate = plan.getStartDate();
 		Date endDate = plan.getEndDate();
-		List<Plan> bookedPlans =  plans.stream()
-											.filter(bookedPlan -> 
-											(startDate.before(bookedPlan.getStartDate())&&endDate.after(bookedPlan.getStartDate()))
-											||(startDate.after(bookedPlan.getStartDate())&&startDate.before(bookedPlan.getEndDate()))
-											||(startDate.before(bookedPlan.getEndDate())&&endDate.after(bookedPlan.getEndDate()))
-											).collect(Collectors.toList());			
-		if((bookedPlans.stream().map(bookedPlan-> bookedPlan.getRoomNumber()).collect(Collectors.toList()).contains(plan.getRoomNumber()))) {			
+		List<Plan> bookedPlans = plans.stream().filter(
+				bookedPlan -> (startDate.before(bookedPlan.getStartDate()) && endDate.after(bookedPlan.getStartDate()))
+						|| (startDate.after(bookedPlan.getStartDate()) && startDate.before(bookedPlan.getEndDate()))
+						|| (startDate.before(bookedPlan.getEndDate()) && endDate.after(bookedPlan.getEndDate())))
+				.collect(Collectors.toList());
+		if ((bookedPlans.stream().map(bookedPlan -> bookedPlan.getRoomNumber()).collect(Collectors.toList())
+				.contains(plan.getRoomNumber()))) {
 			return "Room is already booked for this date";
-		}else if((bookedPlans.stream().map(bookedPlan-> bookedPlan.getTrainerName()).collect(Collectors.toList()).contains(plan.getTrainerName()))) {			
-			return "Trainer is Busy on this date";
-		}		
+		} else if ((bookedPlans.stream().map(bookedPlan -> bookedPlan.getTrainerName()).collect(Collectors.toList())
+				.contains(plan.getTrainerName()))) {
+			return "Trainer is busy on this date";
+		}
 		return "Valid";
 	}
-	
-// ---------ALL FOR CONTROLLER----------
-	
-//	public String createPlan(Plan plan) {
-//	String dateCheck = checkDate(plan);
-//	if(dateCheck.equals("Valid")) {
-//		
-//	}else return dateCheck;
-//}	
-//	public Optional<Plan> getPlan(Long trainerId, Long planId) {
-//		return repo.findById(planId);
-//	}
-//	
-//	public Plan updatePlan(Long planId, int roomNumber, String traineeGroup, String topic) {
-//		Plan plan = new Plan();
-//		plan.setRoomNumber(roomNumber);
-//		plan.setTraineeGroup(traineeGroup);
-//		plan.setTopic(topic);
-//		return repo.save(plan);
-//	}
-//	
-//	public List<Plan> getAllPlans() {
-//		return repo.findAll();
-//	}
-//	
-//	public String deletePlan(Long planId) {
-//		
-//		repo.deleteById(planId);
-//		return "Plan Deleted"; 
-//	}
 
-}   
+}
--- src/test/java/com/qa/plan/Business/serviceTests/PlanServiceTest.java
+++ src/test/java/com/qa/plan/Business/serviceTests/PlanServiceTest.java
@@ -2,10 +2,9 @@ package com.qa.plan.Business.serviceTests;
 
 import static org.junit.Assert.assertEquals;
 
-import java.time.Month;
-import java.time.Year;
-import java.util.Calendar;
-import java.util.Optional;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,7 +15,6 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.qa.plan.CONSTANTS;
 import com.qa.plan.PlanControllerApplication;
 import com.qa.plan.entities.Plan;
 import com.qa.plan.repository.PlanRepository;
@@ -27,32 +25,97 @@ import com.qa.plan.service.PlanService;
 @SpringBootTest(classes = PlanControllerApplication.class)
 public class PlanServiceTest {
 
-		private static final Long testLong = 1L;
-		private static final String testString = "test";
+		private static final Plan plan2 = new Plan();
+		private static final Plan plan1  = new Plan();
+		private static final Plan clashPlan = new Plan();
+		private static final Date startDate1 = new Date(119, 5, 1);
+		private static final Date endDate1 = new Date(119, 11, 1);
+		private static final Date startDate2 = new Date(119, 1, 1);
+		private static final Date endDate2 = new Date(119, 8, 1);
+		private static final Date clashStart = new Date(119, 6, 1);
+		private static final Date clashEnd = new Date(119, 7, 1);
+		private static List<Plan> plans;
 		
 		@InjectMocks
 		PlanService svc;
 
 		@Mock
 		PlanRepository repo;
+		
+		@Mock
+		Plan plan;
 
 		@Before
 		public void setup() {
-
+			plan2.setEndDate(endDate1);
+			plan2.setStartDate(startDate1);
+			plan2.setRoomNumber(7);
+			plan2.setTrainerName("Jordan Harrison");
+			plan1.setEndDate(endDate2);
+			plan1.setStartDate(startDate2);
+			plan1.setRoomNumber(5);
+			plan1.setTrainerName("Chester Gardner");
+			clashPlan.setEndDate(clashEnd);
+			clashPlan.setStartDate(clashStart);
+			clashPlan.setRoomNumber(6);
+			clashPlan.setTrainerName("Jordan Harrison");
+			
+			plans = new ArrayList<Plan>();
+			plans.add(plan2);
+			
 		}
 		
 		@Test
-		public void testCreatePlan() {
-			int month = 7;
-			System.out.println(Calendar.MONTH);
-//			Mockito.when(repo.save(Mockito.any())).thenReturn(CONSTANTS.MOCK_OBJECT);
-//			assertEquals(CONSTANTS.MOCK_OBJECT, svc.createPlan(CONSTANTS.TEST_LONG, "" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH), "" + Calendar.getInstance().get(Calendar.MONTH), "" + Calendar.getInstance().get(Calendar.YEAR), CONSTANTS.TEST_INT, CONSTANTS.TEST_STRING, CONSTANTS.TEST_STRING));
+		public void testCheckDate1() {
+			Mockito.when(plan.getStartDate()).thenReturn(startDate1);
+			Mockito.when(plan.getEndDate()).thenReturn(endDate1);
+			assertEquals(svc.checkDate(plan),"Valid");
 		}
 		
-//		@Test
-//		public void testGetPlan() {
-//			Mockito.when(repo.findById(CONSTANTS.TEST_LONG)).thenReturn(CONSTANTS.MOCK_OPTIONAL_OBJECT);
-//			assertEquals(CONSTANTS.MOCK_OPTIONAL_OBJECT, svc.getPlan(CONSTANTS.TEST_LONG, CONSTANTS.TEST_LONG));
-//		}
+		@Test
+		public void testCheckDate2() {
+			Mockito.when(plan.getStartDate()).thenReturn(startDate2);
+			assertEquals("Date in past!", svc.checkDate(plan));
+		}
+		
+		@Test
+		public void testCheckDate3() {
+			Mockito.when(plan.getStartDate()).thenReturn(endDate2);
+			Mockito.when(plan.getEndDate()).thenReturn(startDate2);
+			assertEquals("End date before start date!", svc.checkDate(plan));
+		}
+		
+		@Test
+		public void testCheckAvailable() {
+			assertEquals(svc.checkAvailible(plan1, plans), "Valid");
+		}
+		
+		@Test
+		public void testCheckAvailable2() {
+			plan1.setRoomNumber(7);
+			assertEquals(svc.checkAvailible(plan1, plans), "Room is already booked for this date");
+		}
+		
+		@Test
+		public void testCheckAvailable3() {
+			
+			assertEquals(svc.checkAvailible(clashPlan, plans), "Trainer is busy on this date");
+		}
+		
+		@Test
+		public void testCheckValid() {
+			assertEquals(svc.checkValid(plan2, plans),"Valid");
+		}
+				
+		@Test
+		public void testCheckValid2() {
+			assertEquals(svc.checkValid(plan1, plans),"Date in past!");
+		}
+		
+		@Test
+		public void testCheckValid3() {
+			assertEquals(svc.checkValid(clashPlan, plans),"Trainer is busy on this date");
+		}
+
 	} 
 
